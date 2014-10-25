2dd7c53da42998c7112f6fe1902ea6b557bdd7bd
Gustavo Gondim <gustavospgondim@gmail.om>
2014-10-25 21:30:34 -0200

    Added files for version 1.0

---
+++ listrap.js
@@ -0,0 +1,24 @@
+// Listrap v1.0, by Gustavo Gondim (http://github.com/ggondim)
+// Licenced under MIT License
+// For updates, improvements and issues, see https://github.com/inosoftbr/listrap
+jQuery.fn.extend({
+    listrap: function () {
+        var listrap = this;
+        $(this).addClass("listrap");
+        listrap.getSelection = function () {
+            var selection = new Array();
+            listrap.children("li.active").each(function (ix, el) {
+                selection.push($(el)[0]);
+            });
+            return selection;
+        }
+        var toggle = "li .listrap-toggle ";
+        var selectionChanged = function () {
+            $(this).parent().parent().toggleClass("active");
+            listrap.trigger("selection-changed", [listrap.getSelection()]);
+        }
+        $(listrap).find(toggle + "img").on("click", selectionChanged);
+        $(listrap).find(toggle + "span").on("click", selectionChanged);
+        return listrap;
+    }
+});
